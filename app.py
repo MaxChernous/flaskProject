41c52aa3c852f16e3505d032414e7dba2b8e07e9
2379 <60963248+2379@users.noreply.github.com>
2022-04-19 15:10:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,9 @@ import flask
 
 app = Flask(__name__)
 
-
-# максим, ты можешь перезапускать пргу?
-# да
-# а сайт у тебя работает?
-# ты на вай фае?
-# классно
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return flask.render_template('register.html')
-
-
-# куда хочешь bruh, у тебя полный доступ к проекту
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
