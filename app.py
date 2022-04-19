e72b68920296c999040ca0c064d19dd9f6d636bc
Foam0 <95540295+Foam0@users.noreply.github.com>
2022-04-19 17:54:32 +0300

    almost nothing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 database = client.test_database
 tasks = database.tasks
-
+# tasks.delete_many({})
 
 
 app = Flask(__name__)
@@ -19,15 +19,15 @@ def home():
     return flask.render_template('register.html')
 
 
-@app.route('/action_page.php', methods=['GET', 'POST'])
+@app.route('/reg', methods=['GET', 'POST'])
 def reg_parse():
     name = (request.values['login'])
     key = (request.values['psw'])
-    if not list(tasks.find({"name": name})) != []:
+    if not tasks.count_documents({"name": name}):
         resp = make_response(flask.render_template("main.html"))
         cookie = str(random.randint(-(10 ** 200), 10 ** 200))
         resp.set_cookie('userID', cookie)
-        tasks.insert_one({"name":name, "key": key})
+        tasks.insert_one({"name": name, "key": key})
         return resp
     else:
         return "вы уже зарегестрированы"
