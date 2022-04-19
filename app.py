e6e1f646744b7ded31731f14fb6afc3d8e1cc1da
Foam0 <95540295+Foam0@users.noreply.github.com>
2022-04-19 13:44:10 +0300

    main started

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ import flask
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return flask.render_template('register.html')
+
+
+@app.route('/scrum')
+def main():
+    return flask.render_template('main.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
