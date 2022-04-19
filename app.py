5921e6f54785ac892b0991e84319b44c35b8a547
Foam0 <95540295+Foam0@users.noreply.github.com>
2022-04-19 14:00:04 +0300

    main started

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,10 +10,18 @@ def home():
     return flask.render_template('register.html')
 
 
+@app.route('/', methods=['GET', 'POST'])
+def parse_request():
+    data = request.data  # data is empty
+    # need posted data here
+
+
 @app.route('/scrum')
 def main():
     return flask.render_template('main.html')
 
+
+# @app.get()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
