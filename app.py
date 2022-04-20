454fccb579d4d4a0b149c8c0f719b33ae9ed38c6
forIinRanger <86529928+forIinRanger@users.noreply.github.com>
2022-04-20 20:10:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,21 @@ from flask import Flask, request
 from jinja2 import FileSystemLoader, Environment
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
+#
 client = MongoClient('localhost', 27017)
 database = client.test_database
 tasks = database.tasks
 boards = database.board
+#
 
 app = Flask(__name__)
 
 
+#
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return flask.render_template('gt.html')
-
+#
 
 @app.route('/reg', methods=['GET', 'POST'])
 def reg_parse():
@@ -33,7 +35,7 @@ def reg_parse():
         resp += "<p class=text>извините данный логин уже занят</p>"
     return resp
 
-
+#
 @app.route('/auth', methods=['GET', 'POST'])
 def authorize():
     name = (request.values['login'])
@@ -46,17 +48,17 @@ def authorize():
             return flask.redirect("/list")
         return "нет пароля"
     return "нет логина"
-
+#
 
 @app.route("/form_reg")
 def reg_send():
     return flask.render_template("register.html")
 
-
+#
 @app.route("/form_auth")
 def auth_send():
     return flask.render_template("authorize.html")
-
+#
 
 @app.route('/list')
 def main():
@@ -64,12 +66,13 @@ def main():
     try: lst = boards.find({"users":  userID})
     except IndexError: lst = []
     return flask.render_template("main.html", lst = lst)
-
+#
 @app.route('/list/add', methods = ['POST'])
 def add():
     userID = ObjectId(request.cookies.get("userID"))
     boards.insert_one({"name": request.form.get("board_name"), "users": [userID]})
     return flask.redirect('/list')
-
+#
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
+#
