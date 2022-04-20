d109bef66e9a19242116d01c5cfb9cc9c6a796ab
2379 <sed.2ok@gmail.com>
2022-04-20 20:13:11 +0500

    ok

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import flask
 from flask import Flask, request
 from jinja2 import FileSystemLoader, Environment
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+
+def _64():
+    alpf = 'abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890#$'
+    n = ''
+    for i in range(10): n+=alpf[random.randint(0, 63)]
+    return n
 
 client = MongoClient('localhost', 27017)
 database = client.test_database
@@ -24,9 +29,9 @@ def reg_parse():
     name = (request.values['login'])
     key = (request.values['psw'])
     if not tasks.count_documents({"name": name}):
-        tasks.insert_one({"name": name, "key": key})
+        tasks.insert_one({"id": _64(), "name": name, "key": key})
         resp = make_response(flask.render_template("main.html"))
-        cookie = str(tasks.find({"name": name})[0]["_id"])
+        cookie = tasks.find_one({"name": name})["id"]
         resp.set_cookie('userID', cookie)
     else:
         resp = (flask.render_template("register.html"))
@@ -39,12 +44,12 @@ def authorize():
     name = (request.values['login'])
     key = (request.values['psw'])
     if tasks.count_documents({"name": name}):
-        if tasks.find({"name": name})[0]["key"] == key:
-            resp = make_response(flask.render_template("main.html"))
-            cookie = str(tasks.find({"name": name})[0]["_id"])
+        if tasks.find_one({"name": name})["key"] == key:
+            resp = make_response(flask.redirect("/list"))
+            cookie = tasks.find_one({"name": name})["id"]
             resp.set_cookie('userID', cookie)
-            return flask.redirect("/list")
-        return "нет пароля"
+            return resp
+        return "неверный пароль"
     return "нет логина"
 
 
@@ -60,16 +65,23 @@ def auth_send():
 
 @app.route('/list')
 def main():
-    userID = ObjectId(request.cookies.get("userID"))
+    userID = request.cookies.get("userID")
     try: lst = boards.find({"users":  userID})
     except IndexError: lst = []
     return flask.render_template("main.html", lst = lst)
 
 @app.route('/list/add', methods = ['POST'])
 def add():
-    userID = ObjectId(request.cookies.get("userID"))
-    boards.insert_one({"name": request.form.get("board_name"), "users": [userID]})
+    userID = request.cookies.get("userID")
+    boards.insert_one({"id": _64(), "name": request.form.get("board_name"), "users": [userID]})
     return flask.redirect('/list')
 
+@app.route('/list/new_user', methods = ['GET'])
+def new_user(): # new user in list 
+    userID = request.cookies.get("userID")
+    (boards.find_one({"id": request.form.get("id")})["users"]).append(userID)
+    return flask.redirect('/list')
+
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
