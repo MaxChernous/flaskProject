21b877b8658cc7aab324529602086445e48b9e66
Foam0 <95540295+Foam0@users.noreply.github.com>
2022-04-21 09:15:53 +0300

    я хочу убить фронта

--- app.py
+++ app.py
@@ -4,23 +4,27 @@ import flask
 from flask import Flask, request
 from pymongo import MongoClient
 
+
 def _64():
     alpf = 'abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ1234567890_.'
     n = ''
-    for i in range(10): n+=alpf[random.randint(0, 63)]
+    for i in range(10): n += alpf[random.randint(0, 63)]
     return n
+
 
 client = MongoClient('localhost', 27017)
 database = client.test_database
 tasks = database.tasks
 boards = database.board
-
+# tasks.delete_many({})
+# boards.delete_many({})
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return flask.render_template('gt.html')
+
 
 @app.route('/reg', methods=['GET', 'POST'])
 def reg_parse():
@@ -36,6 +40,7 @@ def reg_parse():
         resp += "<p class=text>извините данный логин уже занят</p>"
     return resp
 
+
 @app.route('/auth', methods=['GET', 'POST'])
 def authorize():
     name = (request.values['login'])
@@ -43,39 +48,47 @@ def authorize():
     if tasks.count_documents({"name": name}):
         if tasks.find_one({"name": name})["key"] == key:
             resp = make_response(flask.redirect("/list"))
-            cookie = tasks.find_one({"name": name})["id"]
+            cookie = tasks.find_one({"name": name})[0]["id"]
             resp.set_cookie('userID', cookie)
             return resp
         return "неверный пароль"
     return "нет логина"
 
+
 @app.route("/form_reg")
 def reg_send():
     return flask.render_template("register.html")
+
 
 @app.route("/form_auth")
 def auth_send():
     return flask.render_template("authorize.html")
 
+
 @app.route('/list')
 def main():
     userID = request.cookies.get("userID")
-    try: lst = boards.find({"users":  userID})
-    except IndexError: lst = []
-    return flask.render_template("main.html", lst = lst)
+    try:
+        lst = boards.find({"users": userID})
+    except IndexError:
+        lst = []
+    return flask.render_template("main.html", lst=lst)
 
-@app.route('/list/add', methods = ['POST'])
+
+@app.route('/list/add', methods=['POST'])
 def add():
     userID = request.cookies.get("userID")
     boards.insert_one({"id": _64(), "name": request.form.get("board_name"), "users": [userID]})
     return flask.redirect('/list')
 
-@app.route('/list/new_user', methods = ['get'])
-def new_user(): # new user in list 
+
+@app.route('/list/new_user', methods=['get'])
+def new_user():  # new user in list
     userID = request.cookies.get("userID")
     boardID = request.args.get("id")
     boards.update_one({"id": boardID}, {"$push": {"users": userID}})
     return flask.redirect('/list')
-    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
